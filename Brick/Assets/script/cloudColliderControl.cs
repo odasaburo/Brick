726bdce08e0c4c5402e557f4e27f28bca8f69b72
odasaburo <odasaburoo@gmail.com>
2015-11-27 20:25:08 +0000

    break
    added functions

---
+++ Brick/Assets/script/cloudColliderControl.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CircleCollider2D))]
+public class cloudColliderControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<CircleCollider2D>().radius = Screen.width / 30;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
