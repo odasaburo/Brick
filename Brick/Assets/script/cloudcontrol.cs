50bfdb438da0d9008121ccd75285861bc118b684
odasaburo <odasaburoo@gmail.com>
2015-12-01 09:05:34 +0000

    Break
    changed lighting object rigibody

--- Brick/Assets/script/cloudcontrol.cs
+++ Brick/Assets/script/cloudcontrol.cs
@@ -8,6 +8,7 @@ public class cloudcontrol : MonoBehaviour {
     public GameObject parent;
     public GameObject gamenmanager;
     public AudioSource audiosource;
+    GameObject[] lighting;
     Animator animator;
 	// Use this for initialization
 	void Start () {
@@ -15,6 +16,7 @@ public class cloudcontrol : MonoBehaviour {
         GetComponent<BoxCollider2D>().size = new Vector2(Screen.width / 11, Screen.width / 14);
         animator = GetComponent<Animator>();
         gamenmanager = GameObject.FindGameObjectWithTag("GameManager");
+        
     }
 
     public void setPosition(float x, float y) {
@@ -22,12 +24,33 @@ public class cloudcontrol : MonoBehaviour {
     }
 	// Update is called once per frame
 	void Update () {
-	
+        lighting = GameObject.FindGameObjectsWithTag("lighting");
+        if(lighting == null)
+        {
+            return;
+        }
+        else
+        {
+            for(int i = 0; i < lighting.Length; i++)
+            {
+                if(lighting[i].GetComponent<Transform>().position.x < GetComponent<Transform>().position.x + Screen.width / 22 &&
+                   lighting[i].GetComponent<Transform>().position.x > GetComponent<Transform>().position.x - Screen.width / 22 &&
+                   lighting[i].GetComponent<Transform>().position.y < GetComponent<Transform>().position.y + Screen.width / 28 &&
+                   lighting[i].GetComponent<Transform>().position.y > GetComponent<Transform>().position.y - Screen.width / 28)
+                {
+                    audiosource.Play();
+                    animator.SetTrigger("destroy");
+                    gamenmanager.GetComponent<Maincontrol>().game_score += 1;
+                    gamenmanager.GetComponent<Maincontrol>().destroy_pos = GetComponent<Transform>().position;
+                    Destroy(parent, 0.7f);
+                }
+            }
+        }
 	}
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.tag == "ball" || other.tag == "lighting")
+        if(other.tag == "ball" )
         {
             audiosource.Play();
             animator.SetTrigger("destroy");
